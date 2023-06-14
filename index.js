fd481e520d79652cab7b967d642d8ab3f7345a4f
Tawsif Adnan <tawsif.adnan22@gmail.com>
2023-06-14 07:57:26 +0600

    tasks get and task delete api created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -27,9 +27,21 @@ async function run() {
 
     const tasksCollection = client.db("taskManagementDB").collection("tasks");
 
+    app.delete("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await tasksCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/tasks", async (req, res) => {
+      const result = await tasksCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/tasks", async (req, res) => {
       const task = req.body;
-      const query = { taskName: task.taskName };
+      const query = { taskTitle: task.taskTitle };
       const existingTask = await tasksCollection.findOne(query);
 
       if (existingTask) {
